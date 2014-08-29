bb7e4f14ce2754baf3810e21181dea5fef0c3018
Flaiker <flaikerfl@gmail.com>
2014-08-29 20:34:43 +0200

    Whitespace auf Leerzeichen von Tab geändert

--- desktop/src/com/jalfsoftware/jalf/desktop/DesktopLauncher.java
+++ desktop/src/com/jalfsoftware/jalf/desktop/DesktopLauncher.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jalfsoftware.jalf.Jalf;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+    public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "to be named";
         config.width = 800;
         config.height = 600;
         new LwjglApplication(new Jalf(), config);
-	}
+    }
 }
