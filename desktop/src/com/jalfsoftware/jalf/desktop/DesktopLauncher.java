87c946dbd6cbef53a642efb8edec2abbdf93eaee
Flaiker <flaikerfl@gmail.com>
2014-08-29 19:30:49 +0200

    Initial Projekt-Setup

---
+++ desktop/src/com/jalfsoftware/jalf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jalfsoftware.jalf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jalfsoftware.jalf.Jalf;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Jalf(), config);
+	}
+}
