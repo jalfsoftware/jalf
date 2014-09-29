347bc42a754dc8b1444601faf73d4d75e485c2b8
Flaiker <flaikerfl@gmail.com>
2014-09-29 21:27:02 +0200

    Das Laden von Maps refactored, jetzt außerhalb des GameScreens, Spielerspawnposition wird jetzt von der Map geladen

--- core/src/com/jalfsoftware/jalf/screens/GameScreen.java
+++ core/src/com/jalfsoftware/jalf/screens/GameScreen.java
@@ -1,11 +1,7 @@
 package com.jalfsoftware.jalf.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
 import com.jalfsoftware.jalf.Jalf;
 import com.jalfsoftware.jalf.entities.AbstractEntity;
 import com.jalfsoftware.jalf.entities.Player;
@@ -21,31 +17,22 @@ public class GameScreen extends AbstractScreen {
     public static final float UNITSCALE            = 0.75f; // Skalierungskonstante für die Darstellung von Maps und Entitäten
     public static final float GRAVITATION_CONSTANT = 0.2f;
 
-    private OrthogonalTiledMapRenderer mapRenderer;
-    private Player                     player;
-    private TiledMap                   currentMap;
-    private TiledMapTileLayer          collisionLayer;
-
+    private Map                  map;
+    private Player               player;
     private List<AbstractEntity> entityList;
     private long                 startTime;
 
-    // Map-Strings
-    private static final String TILE_BLOCKED_KEY     = "blocked";
-    private static final String COLLISION_LAYER_NAME = "mgLayer";
-
     public GameScreen(Jalf jalf, Map map) {
         super(jalf);
+        this.map = map;
 
         // Maprenderer initialisieren
         Gdx.app.log(LOG, "Loading " + (map.isDefault() ? "defaultmap " : "usermap ") + map.getName() + " from " + map.getPath());
-        currentMap = new TmxMapLoader().load(map.getPath());
-
-        mapRenderer = new OrthogonalTiledMapRenderer(currentMap, UNITSCALE);
-        collisionLayer = (TiledMapTileLayer) currentMap.getLayers().get(COLLISION_LAYER_NAME);
+        boolean mapIsValid = map.loadMap();
+        // TODO: rausspringen, wenn Map nicht korrekt aufgebaut
 
         // Spieler initialisieren
-        Vector2 spawnPosition = convertToScreenPosition(new Vector2(3, 12));
-        player = new Player(spawnPosition.x, spawnPosition.y, 10, 10, 20, 5, 5, this);
+        player = new Player(map.getSpawnPosition().x * UNITSCALE, map.getSpawnPosition().y * UNITSCALE, 10, 10, 20, 5, 5, this);
 
         // Gegnerliste initialisieren
         entityList = new ArrayList<AbstractEntity>();
@@ -70,61 +57,21 @@ public class GameScreen extends AbstractScreen {
         if (player.getX() > SCREEN_WIDTH / 2) camera.position.x = player.getX();
         else camera.position.x = SCREEN_WIDTH / 2;
         camera.update();
-        mapRenderer.setView(camera);
-        mapRenderer.render();
 
-        mapRenderer.getSpriteBatch().begin();
+        OrthogonalTiledMapRenderer renderer = map.getRenderer();
+        renderer.setView(camera);
+        renderer.render();
+
+        renderer.getSpriteBatch().begin();
 
         // Spieler rendern
-        player.render(mapRenderer.getSpriteBatch());
+        player.render(renderer.getSpriteBatch());
 
-        mapRenderer.getSpriteBatch().end();
+        renderer.getSpriteBatch().end();
     }
 
-    /**
-     * Prüft, ob das Tile im Kollisionslayer an der übergebenen Position auf der Map blockiert ist (Kollisionsabfrage)
-     */
-    public boolean isPositionBlocked(int x, int y) {
-        TiledMapTileLayer.Cell cell = collisionLayer.getCell(x, y);
-        return cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey(TILE_BLOCKED_KEY);
-    }
-
-    /**
-     * Konvertiert eine Screen-Position zu einer Map-Position (1LE=1Tile)
-     */
-    public Vector2 convertToMapPosition(Vector2 screenPosition) {
-        return new Vector2((screenPosition.x / collisionLayer.getTileWidth()) / UNITSCALE,
-                           (screenPosition.y / collisionLayer.getTileHeight()) / UNITSCALE);
-    }
-
-    /**
-     * Konvertiert eine Map-Position zu einer Screen-Position (1LE=1Pixel)
-     */
-    public Vector2 convertToScreenPosition(Vector2 mapPosition) {
-        return new Vector2((mapPosition.x * collisionLayer.getTileWidth()) * UNITSCALE,
-                           (mapPosition.y * collisionLayer.getTileHeight()) * UNITSCALE);
-    }
-
-    /**
-     * Konvertiert ScreenUnits zu MapUnits
-     */
-    public float convertToMapUnits(float screenUnits) {
-        return screenUnits / collisionLayer.getTileWidth() / UNITSCALE;
-    }
-
-    /**
-     * Konvertiert MapUnits zu ScreenUnits
-     */
-    public float convertToScreenUnits(float mapUnits) {
-        return mapUnits * collisionLayer.getTileWidth() * UNITSCALE;
-    }
-
-    public float getMapTileHeight() {
-        return collisionLayer.getTileHeight();
-    }
-
-    public float getMapTileWidth() {
-        return collisionLayer.getTileWidth();
+    public Map getMap() {
+        return map;
     }
 
     @Override
